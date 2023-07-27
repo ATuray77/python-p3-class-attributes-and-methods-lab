0feaf67f904fd5ee36781788947fe11456d43044
Aruna Turay <tura_arun@yahoo.com>
2023-07-27 19:05:39 -0400

    so far so good

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,49 @@
 class Song:
-    pass
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artist_count = {}
+
+
+    def __init__(self, name, artist, genre):
+        Song.add_song_to_count(self)
+        Song.add_to_genres(genre)
+        Song.add_to_artist(artist)
+        Song.add_to_genre_count(genre)
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+
+    @classmethod
+    def add_song_to_count(cls, song):
+        cls.count += 1
+
+    @classmethod
+    def add_to_genres(cls, genre):
+        if cls.genres.count(genre) == 0:
+            cls.genres.append(genre)
+
+    @classmethod
+    def add_to_artist(cls, artist):
+        if cls.artists.count(artist) == 0:
+            cls.artists.append(artist)
+
+    @classmethod
+    def add_to_genre_count(cls, genre):
+        if cls.genre_count.get(genre) == None:
+            cls.genre_count[genre] = 1
+        else:
+            cls.genre_count[genre] += 1
+
+
+    @classmethod
+    def add_to_artist_count(cls, artist):
+        if cls.artist_count.get(artist) == None:
+            cls.artist_count[artist] = 1
+        else:
+            cls.artist_count[artist] += 1
+
+    
+
+
